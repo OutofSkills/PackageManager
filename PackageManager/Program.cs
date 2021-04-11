a729218db99240d1e3bdb254306a65ad270598f5
OutofSkills <56539829+OutofSkills@users.noreply.github.com>
2021-04-11 19:02:25 +0300

    Task 1 done

--- PackageManager/Program.cs
+++ PackageManager/Program.cs
@@ -1,12 +1,70 @@
 ﻿using System;
-
 namespace PackageManager
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	using System;
+	using System.Collections.Generic;
+
+	public class Program
+	{
+		public static void Main()
+		{
+			/*graph data structure*/
+			Graph graph = new Graph();
+			/*File manager object*/
+			IOFile file = new IOFile();
+			/*List of nodes to be added*/
+			List<Graph.Node> nodes = new List<Graph.Node>();
+
+			/*list of pairs A B parsed from the input file*/
+			List<string[]> list = file.Read();
+
+			foreach(var item in list)
+            {
+				var tempNode = new Graph.Node(item[0]);
+				/*if the node doesn't exist create it and add his neighbors*/
+				if (!nodes.Exists(n => n.val == tempNode.val))
+				{
+					nodes.Add(tempNode);
+					tempNode.neighbors = new List<Graph.Node> { };
+
+					if(!nodes.Exists(n => n.val == item[1]))
+                    {
+						var neighbor = new Graph.Node(item[1]);
+						nodes.Add(neighbor);
+						tempNode.neighbors.Add(neighbor);
+					}
+					else
+                    {
+						var index = nodes.FindIndex(n=>n.val == item[1]);
+						tempNode.neighbors.Add(nodes[index]);
+                    }
+				}
+				else/*if the node already exists, just add the neighbors*/
+                {
+					var index = nodes.FindIndex(n => n.val == item[0]);
+
+					if (!nodes.Exists(n => n.val == item[1]))
+					{
+						var neighbor = new Graph.Node(item[1]);
+						nodes.Add(neighbor);
+						nodes[index].neighbors.Add(neighbor);
+					}
+					else
+					{
+						var i = nodes.FindIndex(n => n.val == item[1]);
+						nodes[index].neighbors.Add(nodes[i]);
+					}
+				}
+			}
+
+			/*Traverse the graph and store the node's values to a list*/
+			graph.Dfs(nodes);
+			/*sort the list*/
+			graph._list.Sort();
+
+			/*write the data to the output file*/
+			file.Write(graph._list);
+
+		}
+	}
 }
